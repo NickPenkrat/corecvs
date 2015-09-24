7e95b1415d93fe5e6d525d627509a23bbf5f9c10
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2015-09-24 16:47:20 +0300

    Added writeback and reading RGB24 for buffer reader provider

--- wrappers/opencv/openCvFileReader.cpp
+++ wrappers/opencv/openCvFileReader.cpp
@@ -1,5 +1,6 @@
 #include "openCvFileReader.h"
 #include "openCvKeyPointsWrapper.h"
+#include "OpenCVTools.h"
 
 #include "global.h"
 
@@ -27,6 +28,22 @@ void init_opencv_reader_provider()
     BufferReaderProvider::getInstance().add(new OpenCvBufferReaderProvider);
 }
 
+corecvs::RGB24Buffer OpenCvBufferReader::readRgb(const std::string &s)
+{
+    cv::Mat img = cv::imread(s, CV_LOAD_IMAGE_COLOR);
+    if (!(img.rows && img.cols && img.data))
+    {
+        std::stringstream ss;
+        ss << "\"" << s << "\" is not a valid image file";
+        throw std::invalid_argument(ss.str());
+    }
+    IplImage ip(img);
+    auto* b = OpenCVTools::getRGB24BufferFromCVImage(&ip);
+    corecvs::RGB24Buffer buffer = *b;
+    delete b;
+    return  buffer;
+}
+
 RuntimeTypeBuffer OpenCvBufferReader::read(const std::string &s)
 {
     cv::Mat img = cv::imread(s, CV_LOAD_IMAGE_GRAYSCALE);
@@ -37,4 +54,18 @@ RuntimeTypeBuffer OpenCvBufferReader::read(const std::string &s)
         throw std::invalid_argument(ss.str());
     }
     return convert(img);
+}
+
+void OpenCvBufferReader::writeRgb(const corecvs::RGB24Buffer &buffer, const std::string &s)
+{
+    auto* b = OpenCVTools::getCVImageFromRGB24Buffer(&const_cast<corecvs::RGB24Buffer&>(buffer));
+    cv::Mat mat(b);
+    imwrite(s, mat);
+    delete b;
+}
+
+void OpenCvBufferReader::write(const RuntimeTypeBuffer &buffer, const std::string &s)
+{
+    cv::Mat img = convert(buffer);
+    imwrite(s, img);
 }
