f23ccbbf28ca2c9bb1b94ed060a3d9d421a7a96f
sfedorenko <Sergey.Fedorenko@lanit-tercom.com>
2015-09-30 09:15:34 +0300

    msvc compiler fix

--- core/geometry/conic.cpp
+++ core/geometry/conic.cpp
@@ -8,23 +8,22 @@ Sphere3d::Sphere3d()
 
 Circle3d Sphere3d::intersectWith(const Sphere3d &other)
 {
-
+    return Circle3d();
 }
 
 Circle3d Sphere3d::intersectWith(const Plane3d &plane)
 {
-
+    return Circle3d();
 }
 
 bool Circle2d::intersectWith(const Circle2d &other, Vector2dd &point1, Vector2dd &point2)
 {
-    double d =  (other.c - c).l2Metric();
+    double d = (other.c - c).l2Metric();
     double r1 = other.r;
 
     double x = (d * d - r1 * r1 + r * r) / (2 * d);
 
-
+    return false;
 }
 
 } // namespace corecvs
-
