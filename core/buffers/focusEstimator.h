e0bfa7273cc4294220112dcf81ca26f19a308e3f
sfedorenko <sergey.fedorenko@lanit-tercom.com>
2015-10-02 13:53:29 +0300

    improved FocusEstimator ifc

--- core/buffers/focusEstimator.h
+++ core/buffers/focusEstimator.h
@@ -16,6 +16,23 @@ public:
     /** Returns focus score that's integral over the image
      */
     static int getScore(unsigned char* data, int h, int w, int stride, int method = 0);
+
+#if defined(RGB24Buffer) || defined(CRGB24BUFFER_H_)
+    static int getScore(RGB24Buffer* buffer)
+    {
+        if (buffer == NULL)
+            return 0;
+
+        G8Buffer *gray = buffer->getChannel(ImageChannel::GRAY);
+
+        int score = getScore(gray->data, gray->h, gray->w, gray->stride);
+
+        delete_safe(gray);
+
+        return score;
+    }
+#endif
+
 };
 
 } //namespace corecvs
