9729616f79bc8ccdd3b62485e3febd9babd595f6
sdf <sdfs@sdfg.f>
2015-11-06 15:03:23 +0300

    Add file exists checker

--- test/calibration/main.cpp
+++ test/calibration/main.cpp
@@ -9,11 +9,24 @@
 using namespace std;
 using namespace corecvs;
 
+bool CheckGDriveCalibrationFolderTest()
+{
+    const char* dirGDrive = std::getenv("TOPCON_DIR_GDRIVE");
+    const char* dirRelPath = "/data/tests/calibration/";
+    const char* fileName = "esDistOutDist.json";
+
+    std::stringstream fs;
+    fs << dirGDrive << dirRelPath << fileName;
+
+    return access(fs.str().c_str(), 0) == 0;
+}
+
 int main(int argc, char **argv)
 {
-    if(1)
+    if(CheckGDriveCalibrationFolderTest)
         ::testing::InitGoogleTest(&argc, argv);
         return RUN_ALL_TESTS();
     else
+        CORE_ASSERT_TRUE(0, "There is no test data");
         return 0;
 }
