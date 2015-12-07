bf508d73f8560bb7e7b4be848bd6fa36dfdfdc1f
sfedorenko <Sergey.Fedorenko@lanit-tercom.com>
2015-12-07 10:12:04 +0300

    run fixes for error cases

--- test/calibration/main.cpp
+++ test/calibration/main.cpp
@@ -1,17 +1,12 @@
-#include <stdlib.h>
+#include <string>
 #include <iostream>
 #include "gtest/gtest.h"
 
 #include "global.h"
 
-#include "fixedArray.h"
 #include <QFile>
-#include <QDir>
-
-#include "jsonGetter.h"
 
 using namespace std;
-using namespace corecvs;
 
 bool CheckGDriveCalibrationFolderTest()
 {
@@ -19,14 +14,27 @@ bool CheckGDriveCalibrationFolderTest()
     const char* dirRelPath = "/data/tests/calibration/";
     const char* fileName = "esDistOutDist.json";
 
+    if (dirGDrive == NULL) {
+        cout << "The envvar TOPCON_DIR_GDRIVE is missed" << endl;
+        return false;
+    }
+
     std::stringstream fs;
-    fs << dirGDrive << dirRelPath << fileName;
-    return access(fs.str().c_str(), 0) == 0;
+    fs << dirGDrive << dirRelPath << fileName << ends;
+    string path = fs.str();
+
+    if (!QFile::exists(path.c_str())) {
+        cout << "The file <" << path << "> is missed" << endl;
+        return false;
+    }
+
+    return true;
 }
 
 int main(int argc, char **argv)
 {
-    if(!CheckGDriveCalibrationFolderTest()){
+    if (!CheckGDriveCalibrationFolderTest())
+    {
           CORE_ASSERT_TRUE(0, "There is no test data");
           return 0;
     }
