ec98ed87b97b32d060bf9bac89dee3dac4fc392f
Pavel Vasilev <explosere@ya.ru>
2015-12-28 05:40:21 +0300

    Rewrite debayer color tests.

--- test-core/debayer/main_test_debayer.cpp
+++ test-core/debayer/main_test_debayer.cpp
@@ -10,27 +10,25 @@
 #include "g12Buffer.h"
 #include "ppmLoader.h"
 #include "converters/debayer.h"
+#include "converters/errorMetrics.h"
 
 using namespace std;
 using namespace corecvs;
 
-TEST(Debayer, colorTest)
+
+Vector2d<int> redRegion(0, 0);
+Vector2d<int> greenRegion(180, 0);
+Vector2d<int> blueRegion(90, 0);
+
+int regionSize = 50;
+
+void performTest(int64_t &r_sum, int64_t &g_sum, int64_t &b_sum, G12Buffer* ppm, RGB48Buffer *result)
 {
-    PPMLoader *ppmLoader = new PPMLoader();
-    G12Buffer *ppm = ppmLoader->load("data/testdata/test_debayer.pgm");
-    CORE_ASSERT_TRUE(ppm != NULL, "PPM Image load failed");
-    CORE_ASSERT_TRUE(ppm->verify(), "PPM Image verification failed");
-    Debayer d(ppm);
-    RGB48Buffer* result = new RGB48Buffer(ppm->h, ppm->w, false);
-    d.toRGB48(Debayer::Bilinear, result);
+    double error = ErrorMetrics::Yrmsd(ppm, result, 4);
 
-    Vector2d<int> redRegion(0, 0);
-    Vector2d<int> greenRegion(180, 0);
-    Vector2d<int> blueRegion(90, 0);
+    // TODO: clarify issue with Y-channel extraction yielding results of different brightness
+    CORE_ASSERT_TRUE(error < 15, "Debayer result is erroneous!");
 
-    int regionSize = 50;
-
-    int64_t r_sum = 0, g_sum = 0, b_sum = 0;
     for (int x = redRegion.x(); x < redRegion.x() + regionSize; x++)
         for (int y = redRegion.y(); y < redRegion.y() + regionSize; y++)
         {
@@ -60,8 +58,57 @@ TEST(Debayer, colorTest)
             g_sum += (2 * result->element(y, x).g() - result->element(y, x).r() - result->element(y, x).b());
             b_sum += (2 * result->element(y, x).b() - result->element(y, x).r() - result->element(y, x).g());
         }
-
     CORE_ASSERT_TRUE(b_sum > r_sum + g_sum, "The decoded image has a wrong blue channel");
+}
+
+TEST(Debayer, colorTestNearest)
+{
+    PPMLoader *ppmLoader = new PPMLoader();
+    G12Buffer *ppm = ppmLoader->load("data/testdata/test_debayer.pgm");
+    CORE_ASSERT_TRUE(ppm != NULL, "PPM Image load failed");
+    CORE_ASSERT_TRUE(ppm->verify(), "PPM Image verification failed");
+    Debayer d(ppm);
+    RGB48Buffer* result = new RGB48Buffer(ppm->h, ppm->w, false);
+    d.toRGB48(Debayer::Nearest, result);
+
+    int64_t r_sum = 0, g_sum = 0, b_sum = 0;
+    performTest(r_sum, g_sum, b_sum, ppm, result);
+
+    delete_safe(ppm);
+    delete_safe(ppmLoader);
+    delete_safe(result);
+}
+
+TEST(Debayer, colorTestBilinear)
+{
+    PPMLoader *ppmLoader = new PPMLoader();
+    G12Buffer *ppm = ppmLoader->load("data/testdata/test_debayer.pgm");
+    CORE_ASSERT_TRUE(ppm != NULL, "PPM Image load failed");
+    CORE_ASSERT_TRUE(ppm->verify(), "PPM Image verification failed");
+    Debayer d(ppm);
+    RGB48Buffer* result = new RGB48Buffer(ppm->h, ppm->w, false);
+    d.toRGB48(Debayer::Bilinear, result);
+
+    int64_t r_sum = 0, g_sum = 0, b_sum = 0;
+    performTest(r_sum, g_sum, b_sum, ppm, result);
+
+    delete_safe(ppm);
+    delete_safe(ppmLoader);
+    delete_safe(result);
+}
+
+TEST(Debayer, colorTestAHD)
+{
+    PPMLoader *ppmLoader = new PPMLoader();
+    G12Buffer *ppm = ppmLoader->load("data/testdata/test_debayer.pgm");
+    CORE_ASSERT_TRUE(ppm != NULL, "PPM Image load failed");
+    CORE_ASSERT_TRUE(ppm->verify(), "PPM Image verification failed");
+    Debayer d(ppm);
+    RGB48Buffer* result = new RGB48Buffer(ppm->h, ppm->w, false);
+    d.toRGB48(Debayer::AHD, result);
+
+    int64_t r_sum = 0, g_sum = 0, b_sum = 0;
+    performTest(r_sum, g_sum, b_sum, ppm, result);
 
     delete_safe(ppm);
     delete_safe(ppmLoader);
