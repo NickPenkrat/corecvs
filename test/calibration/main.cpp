b2f476a36f1cf1be0bf42677d67c2cd31116e9d0
sfedorenko <Sergey.Fedorenko@lanit-tercom.com>
2016-01-06 22:41:01 +0300

    added one test to calibration tests

--- test/calibration/main.cpp
+++ test/calibration/main.cpp
@@ -4,38 +4,32 @@
 #include "global.h"
 
 #include <QFile>
+#include <QDir>
 
 using namespace std;
 
-bool CheckGDriveCalibrationFolderTest()
+TEST(CalibrationTest, CheckWorkFolder)
 {
     const char* dirGDrive = std::getenv("TOPCON_DIR");
     if (dirGDrive == NULL) {
-        cout << "The env.var. TOPCON_DIR is missed" << endl;
-        return false;
+        CORE_ASSERT_FAIL("The env.var. TOPCON_DIR is missed!");
     }
-    string path = dirGDrive;
-    if (!STR_HAS_SLASH_AT_END(path)) {
+    QString path(dirGDrive);
+    if (!QSTR_HAS_SLASH_AT_END(path)) {
         path += PATH_SEPARATOR;
     }
 
     path += "data/tests/calibration/esDistOutDist.json";
+    path = QDir::toNativeSeparators(path);
 
-    if (!QFile::exists(path.c_str())) {
-        cout << "The file <" << path << "> is missed" << endl;
-        return false;
+    if (!QFile::exists(path)) {
+        cout << "The work file <" << path.toStdString() << "> is missed" << endl;
+        CORE_ASSERT_FAIL("There is no test data!");
     }
-
-    return true;
 }
 
 int main(int argc, char **argv)
 {
-    if (!CheckGDriveCalibrationFolderTest())
-    {
-          CORE_ASSERT_TRUE(0, "There is no test data");
-          return 0;
-    }
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
