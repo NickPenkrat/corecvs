d82b6337bb8c5a7d2a2731b410de9b3fea0c376f
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2016-01-11 15:42:53 +0300

    Modified full calibration routine, added error computations

--- utils/photostationcalibration/calibrationJob.h
+++ utils/photostationcalibration/calibrationJob.h
@@ -140,6 +140,10 @@ struct CalibrationJob
     std::vector<CameraLocationData>                 calibrationSetupLocations;
     std::vector<std::vector<ImageData>>             observations;
     std::vector<std::vector<CalibrationSetupEntry>> calibrationSetups;
+    double                                          totalFullErrorMax        = -1.0,
+                                                    totalFullErrorRMSE       = -1.0,
+                                                    totalCalibrationErrorMax = -1.0,
+                                                    totalCalibrationErrorRMSE= -1.0;
 
     bool                                            calibrated = false;
 
@@ -154,6 +158,10 @@ struct CalibrationJob
         visitor.visit(observations, "observations");
         visitor.visit(settings, CalibrationSettings(), "algorithmSettings");
         visitor.visit(calibrated, false, "calibrated");
+        visitor.visit(totalFullErrorMax, -1.0, "totalFullErrorMax");
+        visitor.visit(totalFullErrorRMSE, -1.0, "totalFullErrorRMSE");
+        visitor.visit(totalCalibrationErrorMax, -1.0, "totalCalibrationErrorMax");
+        visitor.visit(totalCalibrationErrorRMSE, -1.0, "totalCalibrationErrorRMSE");
     }
 
     static corecvs::RGB24Buffer LoadImage(const std::string& path);
