a496890d09f8ffbdd52719563644c2ba6e74cbdc
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2016-01-11 11:48:53 +0300

    Added map direction checkbox connection

--- utils/distortioncorrector/lensDistortionModelParametersControlWidget.cpp
+++ utils/distortioncorrector/lensDistortionModelParametersControlWidget.cpp
@@ -33,6 +33,7 @@ LensDistortionModelParametersControlWidget::LensDistortionModelParametersControl
     QObject::connect(ui->normalizerSpinBox,  SIGNAL(valueChanged(double)), this, SIGNAL(paramsChanged()));
     QObject::connect(ui->shiftXSpinBox,      SIGNAL(valueChanged(double)), this, SIGNAL(paramsChanged()));
     QObject::connect(ui->shiftYSpinBox,      SIGNAL(valueChanged(double)), this, SIGNAL(paramsChanged()));
+    QObject::connect(ui->forwardMapCheckBox, SIGNAL(valueChanged(double)), this, SIGNAL(paramsChanged()));
 
     QObject::connect(ui->koefTableWidget, SIGNAL(itemChanged(QTableWidgetItem*)), this, SIGNAL(paramsChanged()));
 
