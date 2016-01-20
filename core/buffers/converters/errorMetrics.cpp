37fef0668598d0e463edfe85726ea34d237650d6
Pavel Vasilev <explosere@ya.ru>
2016-01-20 14:17:44 +0300

    hgvc

--- core/buffers/converters/errorMetrics.cpp
+++ core/buffers/converters/errorMetrics.cpp
@@ -1,6 +1,6 @@
 #include "errorMetrics.h"
 #include "debayer.h"
-
+#include "ppmLoader.h"
 using namespace corecvs;
 
 double ErrorMetrics::mse(RGB48Buffer *img1, RGB48Buffer *img2, int border)
@@ -74,7 +74,7 @@ double ErrorMetrics::rmsd(G12Buffer *img1, G12Buffer *img2, int border)
     return MSE == -1 ? -1 : sqrt(MSE);
 }
 
-double ErrorMetrics::Ymse(G12Buffer *bayer, RGB48Buffer *debayer, int border)
+double ErrorMetrics::Ymse(G12Buffer *bayer, RGB48Buffer *debayer, int border, int bits, G12Buffer*y, G12Buffer*y2, int& max, Vector2d<int>& maxpos, double& errint)
 {
     if (bayer->w != debayer->w || bayer->h != debayer->h)
         return -1;
@@ -83,25 +83,42 @@ double ErrorMetrics::Ymse(G12Buffer *bayer, RGB48Buffer *debayer, int border)
     
     // this could be slow
 
-    Debayer d(bayer);
-    G12Buffer *y = new G12Buffer(bayer->h, bayer->w, false);
+    Debayer d(bayer, bits);
     d.getYChannel(y);
+
+    //int max = 0;
 
     for (int i = border; i < bayer->h - border; i++)
     {
         for (int j = border; j < bayer->w - border; j++)
         {
-            err += pow((int32_t)y->element(i, j) - debayer->element(i, j).Y(), 2);
+            double orig = y->element(i, j);
+            auto elem = debayer->element(i, j);
+            double deb = (116.0 * elem.r() + 232.0 * elem.g() + 116.0 * elem.b()) / 464.0;
+            double debint = debayer->element(i, j).yh();
+            double abserr = abs(orig - deb);
+            double abserrint = abs(orig - debint);
+            double localerr = pow(abserr, 2);
+            double localerrint = pow(abserrint, 2);
+            
+            if (max < abserr) {
+                max = abserr;
+                maxpos[maxpos.FIELD_Y] = i;
+                maxpos[maxpos.FIELD_X] = j;
+            }
+
+            err += localerr;
+            errint += pow(y->element(i, j) - debayer->element(i, j).yh(),2);
+
+            y2->element(i, j) = debayer->element(i, j).yh();
         }
     }
-
-    delete_safe(y);
-    
+    errint /= ((bayer->h - 2 * border) * (bayer->w - 2 * border));
     return err / ((bayer->h - 2 * border) * (bayer->w - 2 * border));
 }
 
-double ErrorMetrics::Yrmsd(G12Buffer *bayer, RGB48Buffer *debayer, int border)
+double ErrorMetrics::Yrmsd(G12Buffer *bayer, RGB48Buffer *debayer, int border, int bits, G12Buffer*y, G12Buffer*y2, int& max, Vector2d<int>& maxpos, double& errint)
 {
-    double MSE = Ymse(bayer, debayer, border);
+    double MSE = Ymse(bayer, debayer, border, bits, y, y2, max, maxpos, errint);
     return sqrt(MSE);
 }
