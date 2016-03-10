4d62252c1bd8b591135ce8a5cfc95fc85e3e1691
sfedorenko <Sergey.Fedorenko@lanit-tercom.com>
2016-03-10 09:10:10 +0300

    build fixes

--- tools/vcprojFix/build.bat
+++ tools/vcprojFix/build.bat
@@ -1,6 +1,6 @@
-del *.vcxproj*
+if exist *.vcxproj*	del *.vcxproj*
 
-call ..\..\..\..\..\scripts\windows\qmake.bat -tp vc vcprojFix.pro "CONFIG+=gen_vsproj"
+call ..\..\..\..\scripts\windows\qmake.bat -tp vc vcprojFix.pro "CONFIG+=gen_vsproj"
 
 rd debug
 rd release
