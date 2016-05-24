7106e60978b1b505ff66f18f838c727972d7ef94
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2016-05-24 18:30:51 +0300

    Even closer to rectification

--- test/stereo_generator/stereo_generator.cpp
+++ test/stereo_generator/stereo_generator.cpp
@@ -19,8 +19,26 @@
 std::pair<corecvs::Matrix33, corecvs::Matrix33> getRectifyingTransform(corecvs::FixtureCamera &camL, corecvs::FixtureCamera &camR, const corecvs::Matrix33 &newK)
 {
 	// 1. camL goes to K[I|0]
-	camR.extrinsics.orientation ^= camL.extrinsics.orientation.conjugated();
-	camR.extrinsics.position -= camL.extrinsics.orientation * camL.extrinsics.position;
+	auto R1 = camL.extrinsics.orientation.conjugated(),
+		 R0 = camL.extrinsics.orientation;
+	std::cout << R0 << std::endl;
+	camR.extrinsics.orientation = camR.extrinsics.orientation ^ R1;
+	camR.extrinsics.position = R0 * (camL.extrinsics.position - camR.extrinsics.position);
+
+	camL.extrinsics.orientation = camL.extrinsics.orientation ^ R1;
+	camL.extrinsics.position = R0 * camL.extrinsics.position - R0 * camL.extrinsics.position;
+
+	// 2. Now we want camR go to K[R|(T>0, 0, 0)]
+	auto T = camR.extrinsics.position;
+	auto e1 = T.normalised(),
+		 e2 = corecvs::Vector3dd(-T[1], T[0], 0.0).normalised();
+	auto e3 = e1 ^ e2;
+	std::cout << e1 << std::endl;
+	auto R = corecvs::Matrix33::FromRows(e1, e2, e3);
+	std::cout << R.det() << std::endl;
+	std::cout << R * camR.extrinsics.position << std::endl;
+	CORE_ASSERT_TRUE_S(R.det() > 0.0);
+	return std::make_pair(R, R);
 }
 
 // I do not want to parse names ('cause I accidentially
@@ -60,15 +78,21 @@ int main(int argc, char **argv)
 	double focal = (focals[focals.size() / 2 - 1] + focals[focals.size() / 2] + focals[focals.size() / 2 + 1]) / 3.0;
 	std::cout << "New cameras will all have focal length " << focal << " and size " << maxH << "x" << maxW << " pixels" << std::endl;
 
+	corecvs::Matrix33 Kn(focal, 0.0, maxW / 2.0, 0.0, focal, maxH / 2.0, 0.0, 0.0, 1.0);
+
 	for (auto& fl: fs.fixtures())
-		for (auto& c1: fl->cameras)
+		for (auto& cl: fl->cameras)
 			for (auto& fr: fs.fixtures())
 			{
 				if (fl == fr)
 					continue;
 				for (auto& cr: fr->cameras)
 				{
-					std::cout << "Trying to rectify " << fl->name << c1.nameId << " and " << fr->name << c2.nameId << std::endl;
+					std::cout << fl->location.rotor << " " << cl->extrinsics.orientation << std::endl;
+					std::cout << "Trying to rectify " << fl->name << cl->nameId << " and " << fr->name << cr->nameId << std::endl;
+					auto camL = fl->getWorldCamera(cl),
+						 camR = fr->getWorldCamera(cr);
+					getRectifyingTransform(camL, camR, Kn);
 				}
 			}
     return 0;
