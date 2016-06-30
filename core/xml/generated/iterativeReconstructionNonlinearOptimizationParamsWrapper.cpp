e9d3d98b689b3222d226d5a2b884b8afa9510291
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2016-06-30 14:40:57 +0300

    New defaults

--- core/xml/generated/iterativeReconstructionNonlinearOptimizationParamsWrapper.cpp
+++ core/xml/generated/iterativeReconstructionNonlinearOptimizationParamsWrapper.cpp
@@ -71,7 +71,7 @@ int IterativeReconstructionNonlinearOptimizationParamsWrapper::staticInit()
         (
           IterativeReconstructionNonlinearOptimizationParamsWrapper::POSTAPPENDNONLINEARITERATIONS_ID,
           offsetof(IterativeReconstructionNonlinearOptimizationParamsWrapper, mPostAppendNonlinearIterations),
-          200,
+          2000,
           "postAppendNonlinearIterations",
           "postAppendNonlinearIterations",
           "Post-append non-linear optimization iterations",
@@ -85,7 +85,7 @@ int IterativeReconstructionNonlinearOptimizationParamsWrapper::staticInit()
         (
           IterativeReconstructionNonlinearOptimizationParamsWrapper::FINALNONLINEARITERATIONS_ID,
           offsetof(IterativeReconstructionNonlinearOptimizationParamsWrapper, mFinalNonLinearIterations),
-          2000,
+          10000,
           "finalNonLinearIterations",
           "finalNonLinearIterations",
           "Final non-linear iterations",
