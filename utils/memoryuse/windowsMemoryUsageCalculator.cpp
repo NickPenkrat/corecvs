af30721b6cfa00cfe9eb54f77b45f37611dbbbaa
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2016-09-07 13:20:43 +0300

    Windows API sucks

--- utils/memoryuse/windowsMemoryUsageCalculator.cpp
+++ utils/memoryuse/windowsMemoryUsageCalculator.cpp
@@ -8,7 +8,7 @@ void WindowsMemoryUsageCalculator::getMemoryUsageImpl()
     // nothing here yet
     PROCESS_MEMORY_COUNTERS_EX pmc;
     pmc.cb = sizeof(pmc);
-    GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
+    GetProcessMemoryInfo(GetCurrentProcess(), (PPROCESS_MEMORY_COUNTERS)&pmc, sizeof(pmc));
 
 
     mVirtualSize = pmc.PrivateUsage / 1024.0 / 1024.0;
