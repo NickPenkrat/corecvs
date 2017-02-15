827698e856d3b4480e9ff7be56d834da4dec91fa
sfedorenko <Sergey.Fedorenko@lanit-tercom.com>
2017-02-15 21:50:10 +0300

    add logging in KDE module

--- core/math/kde.cpp
+++ core/math/kde.cpp
@@ -5,25 +5,29 @@
 #include "kde.h"
 #include "rgb24Buffer.h"
 #include "abstractPainter.h"
+#include "log.h"
 
-void corecvs::kde::defaultBandwidth(int currentVariable)
+namespace corecvs {
+
+
+void kde::defaultBandwidth(int currentVariable)
 {
     if (!countMap[currentVariable])
     {
-        std::cout << "No data!" << std::endl;
+        L_ERROR << "No data!";
         exit(1);
     }
-    double x  = sumXMap [currentVariable]/countMap[currentVariable];
-    double x2 = sumX2Map[currentVariable]/countMap[currentVariable];
-    double sigma = sqrt(x2 - (x*x));
-    double b = sigma*(pow((3.0*countMap[currentVariable]/4.0),(-1.0/5.0)));
+    double x = sumXMap[currentVariable] / countMap[currentVariable];
+    double x2 = sumX2Map[currentVariable] / countMap[currentVariable];
+    double sigma = sqrt(x2 - (x * x));
+    double b = sigma * (pow((3.0 * countMap[currentVariable] / 4.0), (-1.0 / 5.0)));
 
-    std::cout << "Bandwidth " << currentVariable << " = " << b << std::endl;
+    L_INFO << "Bandwidth " << currentVariable << " = " << b;
 
     bandwidthMap[currentVariable] = b;
 }
 
-void corecvs::kde::calcBandwidth()
+void kde::calcBandwidth()
 {
     for (int currentVariable = 0; currentVariable < dataMatrix.size(); currentVariable++)
     {
@@ -34,28 +38,28 @@ void corecvs::kde::calcBandwidth()
     }
 }
 
-double corecvs::kde::gaussPDF(double x, double m, double s)
+double kde::gaussPDF(double x, double m, double s)
 {
     double z = (x - m) / s;
     return exp(-0.5 * z * z) / (s * sqrt(2.0 * M_PI));
 }
 
-double corecvs::kde::pdf(double x)
+double kde::pdf(double x)
 {
     std::vector<double> tmp;
     tmp.push_back(x);
-    return(pdf(tmp));
+    return pdf(tmp);
 }
 
-double corecvs::kde::pdf(double x, double y)
+double kde::pdf(double x, double y)
 {
     std::vector<double> tmp;
     tmp.push_back(x);
     tmp.push_back(y);
-    return(pdf(tmp));
+    return pdf(tmp);
 }
 
-double corecvs::kde::pdf(std::vector<double>& data)
+double kde::pdf(std::vector<double>& data)
 {
     calcBandwidth();
     double d = 0.0;
@@ -71,14 +75,14 @@ double corecvs::kde::pdf(std::vector<double>& data)
     return d / countMap[0];
 }
 
-void corecvs::kde::addData(double x)
+void kde::addData(double x)
 {
     std::vector<double> tmp;
     tmp.push_back(x);
     addData(tmp);
 }
 
-void corecvs::kde::addData(double x, double y)
+void kde::addData(double x, double y)
 {
     std::vector<double> tmp;
     tmp.push_back(x);
@@ -86,7 +90,7 @@ void corecvs::kde::addData(double x, double y)
     addData(tmp);
 }
 
-void corecvs::kde::addData(std::vector<double>& x)
+void kde::addData(std::vector<double>& x)
 {
     if (!dataMatrix.size())
     {
@@ -96,7 +100,7 @@ void corecvs::kde::addData(std::vector<double>& x)
             tmp.push_back(x[i]);
             dataMatrix.push_back(tmp);
             sumXMap[i] = x[i];
-            sumX2Map[i] = x[i]*x[i];
+            sumX2Map[i] = x[i] * x[i];
             countMap[i] = 1;
             minMap[i] = x[i];
             maxMap[i] = x[i];
@@ -107,7 +111,7 @@ void corecvs::kde::addData(std::vector<double>& x)
     {
         if (x.size() != dataMatrix.size())
         {
-            std::cout << "Number of variables doesn't match!" << std::endl;
+            L_INFO << "Number of variables doesn't match!";
         }
         else
         {
@@ -115,7 +119,7 @@ void corecvs::kde::addData(std::vector<double>& x)
             {
                 dataMatrix[i].push_back(x[i]);
                 sumXMap[i] += x[i];
-                sumX2Map[i] += x[i]*x[i];
+                sumX2Map[i] += x[i] * x[i];
                 countMap[i]++;
                 minMap[i] = x[i] < minMap[i] ? x[i] : minMap[i];
                 maxMap[i] = x[i] > maxMap[i] ? x[i] : maxMap[i];
@@ -125,21 +129,21 @@ void corecvs::kde::addData(std::vector<double>& x)
     }
 }
 
-std::vector<double> corecvs::kde::calcPDF(int testPointCountX
+std::vector<double> kde::calcPDF(int testPointCountX
     , int testPointCountY
     , double passLevel
     , std::vector<double> minXY
     , std::vector<double> maxXY
-    , corecvs::RGB24Buffer* buffer
-    )
+    , RGB24Buffer* buffer
+)
 {
     double minX = minXY[0];
     double maxX = maxXY[0];
     double minY = minXY[1];
     double maxY = maxXY[1];
 
-    std::cout << "min x,y = " << minX << "," << minY << std::endl;
-    std::cout << "max x,y = " << maxX << "," << maxY << std::endl;
+    L_INFO << "min x,y = " << minX << "," << minY;
+    L_INFO << "max x,y = " << maxX << "," << maxY;
 
     ndX = testPointCountX;
     ndY = testPointCountY;
@@ -149,26 +153,26 @@ std::vector<double> corecvs::kde::calcPDF(int testPointCountX
     double y = minY;
     double x = minX;
 
-    std::cout << "increment x,y = " << xIncrement << "," << yIncrement << std::endl;
+    L_INFO << "increment x,y = " << xIncrement << "," << yIncrement;
 
     calcBandwidth();
 
-    std::cout << "# bandwidth var 1: " << getBandwidth(0) << std::endl;
-    std::cout << "# bandwidth var 2: " << getBandwidth(1) << std::endl;
+    L_INFO << "# bandwidth var 1: " << getBandwidth(0);
+    L_INFO << "# bandwidth var 2: " << getBandwidth(1);
 
     std::vector<double> def;
 
     double min = 0.0;
     double max = 0.0;
 
-    for(int i = 0; i < ndX; i++)
+    for (int i = 0; i < ndX; i++)
     {
         x += xIncrement;
         y = minY;
         for (int j = 0; j < ndY; j++)
         {
             y += yIncrement;
-            def.emplace_back(pdf(x,y));
+            def.emplace_back(pdf(x, y));
             min = def.back() < min ? def.back() : min;
             max = def.back() > max ? def.back() : max;
         }
@@ -184,31 +188,32 @@ std::vector<double> corecvs::kde::calcPDF(int testPointCountX
 
     for (int i = 0; i < ndX; i++)
     {
+        std::ostringstream ss;
         x += xIncrement;
         y = minY;
         for (int j = 0; j < ndY; j++)
         {
             y += yIncrement;
             index++;
-            double norm = (def[index] - min)/(max-min);
+            double norm = (def[index] - min) / (max - min);
             if (norm < passLevel)
                 underPass++;
             def[index] = norm;
-            std::cout << norm << "\t" ;
+            ss << norm << "\t";
 
-            painter.drawFormat(x,  y, RGBColor(0x00FF00), 1, std::to_string(norm).c_str());
+            painter.drawFormat(x, y, RGBColor(0x00FF00), 1, std::to_string(norm).c_str());
         }
-        std::cout << std::endl;
+        L_INFO << ss.str();
     }
 
-    std::cout << "Complete " << std::endl;
+    L_INFO << "Complete";
     return def;
 }
 
-std::vector<double> corecvs::kde::calcPDF(int testPointCountX
+std::vector<double> kde::calcPDF(int testPointCountX
     , int testPointCountY
     , double passLevel
-    , corecvs::RGB24Buffer* buffer)
+    , RGB24Buffer* buffer)
 {
     double minX = getMin(0);
     double maxX = getMax(0);
@@ -216,10 +221,12 @@ std::vector<double> corecvs::kde::calcPDF(int testPointCountX
     double maxY = getMax(1);
 
     return calcPDF(testPointCountX,
-                   testPointCountY,
-                   passLevel,
-                   std::vector<double>({minX, minY}),
-                   std::vector<double>({maxX, maxY}),
-                   buffer
-                   );
+        testPointCountY,
+        passLevel,
+        std::vector<double>({ minX, minY }),
+        std::vector<double>({ maxX, maxY }),
+        buffer
+    );
 }
+
+} // namespace corecvs
