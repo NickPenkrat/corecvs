7b88800ce0aa032507fb5e0b28f3d32895e1db26
sfedorenko <Sergey.Fedorenko@lanit-tercom.com>
2017-02-15 21:50:45 +0300

    build fix changed KDE

--- core/math/kde.h
+++ core/math/kde.h
@@ -13,29 +13,31 @@
 #include "global.h"
 
 namespace corecvs {
-    class kde
-	{
+
+class RGB24Buffer;
+
+
+class kde
+{
 	public:
         void    addData(double x);
         void    addData(double x, double y);
         void    addData(std::vector<double>& x);
 
         std::vector<double>
-                calcPDF(int testPointCountX = 10, int testPointCountY = 10, double passLevel = .3, corecvs::RGB24Buffer* buffer = nullptr);
+                calcPDF(int testPointCountX = 10, int testPointCountY = 10
+                    , double passLevel = .3
+                    , corecvs::RGB24Buffer* buffer = nullptr);
 
         std::vector<double>
-                calcPDF(
-                int testPointCountX,
-                int testPointCountY,
-                double passLevel,
-                std::vector<double> min,
-                std::vector<double> max,
-                corecvs::RGB24Buffer* buffer = nullptr
-                );
+                calcPDF(int testPointCountX, int testPointCountY
+                , double passLevel
+                , std::vector<double> min
+                , std::vector<double> max
+                , corecvs::RGB24Buffer* buffer = nullptr);
 
     private:
-        std::vector<std::vector<double> >
-                dataMatrix;
+        std::vector<std::vector<double> >   dataMatrix;
 
         std::map<int,double>
                 sumXMap,
@@ -46,13 +48,11 @@ namespace corecvs {
                 bandwidthMap;
 
         int     extension;
-
         int     ndX, ndY;
 
-        double  getMin(int x){ return minMap[x]; }
-        double  getMax(int x){ return maxMap[x]; }
-
-        double  getBandwidth(int x){ return(bandwidthMap[x]);}
+        double  getMin(int x)       { return minMap[x]; }
+        double  getMax(int x)       { return maxMap[x]; }
+        double  getBandwidth(int x) { return(bandwidthMap[x]); }
 
         void    calcBandwidth();
         void    defaultBandwidth(int currentVariable);
@@ -60,7 +60,8 @@ namespace corecvs {
         double  pdf(double x);
         double  pdf(double x, double y);
         double  pdf(std::vector<double>& data);
-    };
-} //namespace corecvs
-#endif  //KDE_H_
+};
 
+} //namespace corecvs
+
+#endif  //KDE_H_
