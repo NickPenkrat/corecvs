a7ac29120c062a0696c9335408568606c5ea6299
sfedorenko <serigfed@gmail.com>
2017-08-08 01:49:48 +0300

    added PPMLoader::loadG16(), new class PPMLoaderG16; fixed method BufferFactory::loadG16Bitmap; fixed memory leak in ~BufferFactory().

--- core/buffers/bufferFactory.cpp
+++ core/buffers/bufferFactory.cpp
@@ -25,10 +25,10 @@ BufferFactory* BufferFactory::getInstance()
         sThis.get()->registerLoader(new PPMLoaderG12());
         sThis.get()->registerLoader(new RAWLoader());
         sThis.get()->registerLoader(new BMPLoaderG12());
+        sThis.get()->registerLoaderG16(new PPMLoaderG16());     // specific reader to load 16bits data PPMs
 
         sThis.get()->registerLoader(new PPMLoaderRGB24());
         sThis.get()->registerLoader(new BMPLoaderRGB24());
-
     }
     return sThis.get();
 }
@@ -40,145 +40,64 @@ void BufferFactory::printCaps()
     cout << "BufferFactory knows:" << endl;
 
     cout << "  G12 loader" << endl;
-    for (auto it : factory->mLoadersG12)
-    {
-        cout << "     " << it->name() << endl;
-    }
+    for (auto it : factory->mLoadersG12) cout << "\t" << it->name() << endl;
+
+    cout << "  G16 loader" << endl;
+    for (auto it : factory->mLoadersG16) cout << "\t" << it->name() << endl;
 
     cout << "  RGB24 loader" << endl;
-    for (auto it : factory->mLoadersRGB24)
-    {
-        cout << "     " << it->name() << endl;
-    }
+    for (auto it : factory->mLoadersRGB24) cout << "\t" << it->name() << endl;
 
     cout << "  RuntimeType loader" << endl;
-    for (auto it : factory->mLoadersRuntime)
-    {
-        cout << "     " << it->name() << endl;
-    }
+    for (auto it : factory->mLoadersRuntime) cout << "\t" << it->name() << endl;
 }
 
-/* Use template function instead of */
-
-G12Buffer *BufferFactory::loadG12Bitmap(string name)
+template<typename BufferType>
+BufferType *loadBuffer(string name, vector<BufferLoader<BufferType> *> &loaders)
 {
-    vector<BufferLoader<G12Buffer> *>::iterator it;
-    for (it = mLoadersG12.begin(); it != mLoadersG12.end(); it++)
+    for (auto it : loaders)
     {
-        if (!((*it)->acceptsFile(name)))
+        //SYNC_PRINT(("BufferFactory::load(%s): loader <%s>\n", name.c_str(), it->name().c_str()));
+
+        if (!(it->acceptsFile(name)))
             continue;
 
-        G12Buffer *result = (*it)->load(name);
-        if (result != NULL)
-            return result;
-    }
-
-    return NULL;
-}
-
-RGB24Buffer *BufferFactory::loadRGB24Bitmap(string name)
-{
-    vector<BufferLoader<RGB24Buffer> *>::iterator it;
-    for (it = mLoadersRGB24.begin(); it != mLoadersRGB24.end(); it++)
-    {
-//        SYNC_PRINT(("BufferFactory::loadRGB24Bitmap(%s): loader <%s>\n", name.c_str(), (*it)->name().c_str()));
-
-        if (!((*it)->acceptsFile(name)))
-            continue;
-
-        RGB24Buffer *result = NULL;
+        BufferType *result = NULL;
         try {
-            result = (*it)->load(name);
+            result = it->load(name);
         }
         catch (std::exception &)
         {
-            SYNC_PRINT(("BufferFactory::loadRGB24Bitmap(): loader <%s> violates contract by throwing unexpected exception", (*it)->name().c_str()));
+            SYNC_PRINT(("BufferFactory::load(): loader <%s> violates contract by throwing unexpected exception", it->name().c_str()));
         }
 
         if (result != NULL) {
             return result;
         }
-        else {
-            SYNC_PRINT(("BufferFactory::loadRGB24Bitmap(%s):  loader <%s> agreed to load, but failed\n",
-                        name.c_str(), (*it)->name().c_str()));
-        }
-    }
 
+        SYNC_PRINT(("BufferFactory::load(%s):  loader <%s> agreed to load, but failed\n", name.c_str(), it->name().c_str()));
+    }
     return NULL;
+}
+
+G12Buffer *BufferFactory::loadG12Bitmap(string name)
+{
+    return loadBuffer(name, mLoadersG12);
+}
+
+G12Buffer *BufferFactory::loadG16Bitmap(string name)
+{
+    return loadBuffer(name, mLoadersG16);
+}
+
+RGB24Buffer *BufferFactory::loadRGB24Bitmap(string name)
+{
+    return loadBuffer(name, mLoadersRGB24);
 }
 
 RuntimeTypeBuffer *BufferFactory::loadRuntimeTypeBitmap(std::string name)
 {
-    vector<BufferLoader<RuntimeTypeBuffer> *>::iterator it;
-    for (it = mLoadersRuntime.begin(); it != mLoadersRuntime.end(); it++)
-    {
-        if (!((*it)->acceptsFile(name)))
-            continue;
-
-        RuntimeTypeBuffer *result = (*it)->load(name);
-        if (result != NULL)
-            return result;
-    }
-
-    return NULL;
-}
-
-
-/* TODO: DELETE THIS. BURN IT WITH FIRE */
-G12Buffer *BufferFactory::loadG16Bitmap(string name)
-{
-    if (name.rfind(".pgm") != string::npos)
-    {
-        return PPMLoader().g16BufferCreateFromPPM(name);
-    }
-
-    G12Buffer *result = NULL;
-    FILE *In = NULL;
-    uint32_t h = 0;
-    uint32_t w = 0;
-    uint16_t d = 0;
-    int read = 0;
-
-
-    In = fopen(name.c_str(), "rb");
-    if (In == NULL)
-    {
-        goto exit;
-    }
-
-    read = (int)fread(&h, sizeof(uint32_t), 1, In);
-    if (read != 1) {
-        goto exit;
-    }
-    read = (int)fread(&w, sizeof(uint32_t), 1, In);
-    if (read != 1)
-    {
-        goto exit;
-    }
-
-    if (h > 2000 || w > 2000)
-    {
-        goto exit;
-    }
-    result = new G12Buffer(h,w);
-
-    for (uint32_t i = 0; i < h; ++i) {
-        for (uint32_t j = 0; j < w; ++j) {
-            read = (int)fread(&d, sizeof(uint16_t), 1, In);
-            if (read != 1)
-            {
-                goto exit;
-            }
-            result->element(i,j) = d;
-        }
-    }
-
-exit:
-    if (In != NULL)
-    {
-        fclose(In);
-    }
-    return result;
+    return loadBuffer(name, mLoadersRuntime);
 }
 
 std::vector<std::string> BufferFactory::resolutionsRGB24()
@@ -199,12 +118,19 @@ std::vector<std::string> BufferFactory::resolutionsRuntimeType()
 BufferFactory::~BufferFactory()
 {
     // delete all registered loaders
-    vector<BufferLoader<G12Buffer> *>::iterator it;
-    for (it = mLoadersG12.begin(); it != mLoadersG12.end(); it++)
-    {
-        delete_safe (*it);
-    }
+
+    for (auto it : mLoadersG12) { delete_safe(it); }
     mLoadersG12.clear();
+
+    for (auto it : mLoadersG16) { delete_safe(it); }
+    mLoadersG16.clear();
+
+    for (auto it : mLoadersRGB24) { delete_safe(it); }
+    mLoadersRGB24.clear();
+
+    for (auto it : mLoadersRuntime) { delete_safe(it); }
+    mLoadersRuntime.clear();
+
     //printf("BufferFactory has been destroyed.\n");
 }
 
--- core/fileformats/ppmLoader.cpp
+++ core/fileformats/ppmLoader.cpp
@@ -34,6 +34,48 @@ G12Buffer* PPMLoader::loadG12(string name)
     return loadMeta(name, nullptr);
 }
 
+G12Buffer* PPMLoader::loadG16(string name)
+{
+    if (HelperUtils::endsWith(name, prefix1))   // == pgm
+    {
+        return PPMLoader().g16BufferCreateFromPPM(name);
+    }
+
+    G12Buffer *result = NULL;
+    uint32_t h = 0, w = 0; uint16_t d = 0;
+    FILE *In = fopen(name.c_str(), "rb");
+    if (In == NULL) {
+        goto exit;
+    }
+    int read = (int)fread(&h, sizeof(uint32_t), 1, In);
+    if (read != 1) {
+        goto exit;
+    }
+    read = (int)fread(&w, sizeof(uint32_t), 1, In);
+    if (read != 1) {
+        goto exit;
+    }
+    if (h > 2000 || w > 2000) {
+        goto exit;
+    }
+    result = new G12Buffer(h, w);
+    for (uint32_t i = 0; i < h; ++i) {
+        for (uint32_t j = 0; j < w; ++j) {
+            read = (int)fread(&d, sizeof(uint16_t), 1, In);
+            if (read != 1) {
+                goto exit;
+            }
+            result->element(i, j) = d;
+        }
+    }
+exit:
+    if (In != NULL)
+    {
+        fclose(In);
+    }
+    return result;
+}
+
 G12Buffer* PPMLoader::loadMeta(const string& name, MetaData *metadata)
 {
     if (metadata != nullptr) {
@@ -43,14 +85,12 @@ G12Buffer* PPMLoader::loadMeta(const string& name, MetaData *metadata)
         DOTRACE(("Will load the file %s as PPM (ignoring any available metadata)\n ", name.c_str()));
     }
 
-    G12Buffer *toReturn = g12BufferCreateFromPGM(name, metadata);
-    return toReturn;
+    return g12BufferCreateFromPGM(name, metadata);
 }
 
 RGB48Buffer* PPMLoader::loadRGB(const string& name, MetaData *metadata)
 {
-    RGB48Buffer *toReturn = rgb48BufferCreateFromPPM(name, metadata);
-    return toReturn;
+    return rgb48BufferCreateFromPPM(name, metadata);
 }
 
 G12Buffer* PPMLoader::g12BufferCreateFromPGM(const string& name, MetaData *meta)
