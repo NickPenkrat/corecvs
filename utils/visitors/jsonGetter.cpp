adcd0af9a5a57c8012db3646ad34a28479876bb0
SerFed <serigfed@gmail.com>
2017-08-17 13:11:12 +0300

    JSONGetter gets method hasError() - to know if there are any errors appeared

--- utils/visitors/jsonGetter.cpp
+++ utils/visitors/jsonGetter.cpp
@@ -14,17 +14,20 @@ void JSONGetter::init(const char *fileName)
     if (file.open(QFile::ReadOnly))
     {
         QByteArray array = file.readAll();
+        QJsonParseError parseError;
+        QJsonDocument document = QJsonDocument::fromJson(array, &parseError);
 
-        QJsonDocument document = QJsonDocument::fromJson(array);
         if (document.isNull())
         {
-            SYNC_PRINT(("Fail parsing the data from <%s>\n", QSTR_DATA_PTR(mFileName)));
+            SYNC_PRINT(("Fail parsing the data from <%s> with error \"%s\"\n\n", QSTR_DATA_PTR(mFileName), parseError.errorString().toStdString().c_str()));
+            mHasError = true;
         }
         object = document.object();
         file.close();
     }
     else {
-        qDebug() << "JSONGetter::init() : Can't open file <" << QSTR_DATA_PTR(mFileName) << ">";
+        SYNC_PRINT(("JSONGetter: couldn't open file <%s>", QSTR_DATA_PTR(mFileName)));
+        mHasError = true;
     }
 
     mNodePath.push_back(object);
