6b652d015f346b3bd129f61f18d1c234e3c606fa
alexander <Aleksandr.Pimenov@lanit-tercom.com>
2017-11-15 13:59:57 +0300

    Minor fixes

--- core/buffers/memory/alignedMemoryBlock.h
+++ core/buffers/memory/alignedMemoryBlock.h
@@ -4,8 +4,10 @@
 #include <memory>
 
 #if defined(WIN32)
+# define aligned_alloc(a, sz)   _aligned_malloc(sz, a)
 # define aligned_free(p)        _aligned_free(p)
 #else
+//# define aligned_alloc(a, sz) // is present at clib
 # define aligned_free(p)        free(p)
 #endif
 
